c93832c90e30f91bb8a1fe0ee51b0a9e45e25c26
Idrianan <47890348+Idrianan@users.noreply.github.com>
2023-07-13 07:20:58 +0300

    Created file for locators and tests for login_page
    Created file for locators and tests for login_page

--- conftest.py
+++ conftest.py
@@ -1,10 +1,20 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en  ',
+                     help="Please, choose language")
+    
 
 @pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
+def browser(request):
+    print("\nstart browser for test..")
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
