a497d76eae9b18f4544121bbd29e71e658e2fa04
Idrianan <47890348+Idrianan@users.noreply.github.com>
2023-07-16 04:22:24 +0300

    Final commit
    fixed some bugs and added pytest.ini to register mark.

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,9 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
-import pytest
 
-@pytest.mark.login_guest
 class TestLoginFromMainPage():
-    # не забываем передать первым аргументом self                       
+
     def test_guest_can_go_to_login_page(self,browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser,link)
@@ -18,28 +16,6 @@ class TestLoginFromMainPage():
         page.open()
         page.should_be_login_link()
 
-
-@pytest.mark.login
-class TestLoginFromProductPage():
-    @pytest.fixture(scope="function", autouse=True)
-    def setup(self):
-        self.product = ProductFactory(title = "Best book created by robot")
-        # создаем по апи
-        self.link = self.product.link
-        yield
-        # после этого ключевого слова начинается teardown
-        # выполнится после каждого теста в классе
-        # удаляем те данные, которые мы создали 
-        self.product.delete()
-        
-
-    def test_guest_can_go_to_login_page_from_product_page(self, browser):
-        page = ProductPage(browser, self.link)
-        # дальше обычная реализация теста
-
-    def test_guest_should_see_login_link(self, browser):
-        page = ProductPage(browser, self.link)
-        # дальше обычная реализация теста
 
 
 
--- test_product_page.py
+++ test_product_page.py
@@ -24,7 +24,7 @@ class TestUserAddToBasketFromProductPage:
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_success_message()
-
+    
     @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self,browser):
         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
